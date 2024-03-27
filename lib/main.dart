44ebe83d8da8c16d1445aa736a0f3ef58ff6640d
ALBÉRICO EDUARDO ALVES CAMPOS SOARES <alberico.campos@a.catolica-to.edu.br>
2024-03-27 20:54:08 -0300

    create register page and change widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_farmapp/loginPage/pages/login_page.dart';
+// import 'package:flutter_farmapp/registerPage/pages/register_page.dart';
 
 void main() {
   runApp(const MainApp());
--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_farmapp/homePage/home_page.dart';
 
 class LoginButton extends StatelessWidget {
-  const LoginButton({super.key});
+  final String text;
+  const LoginButton({super.key, required this.text});
 
   @override
   Widget build(BuildContext context) {
+
     return ElevatedButton(
       onPressed: () {
         Navigator.push(
@@ -17,9 +19,9 @@ class LoginButton extends StatelessWidget {
         backgroundColor: const Color.fromARGB(153, 230, 186, 149),
         elevation: 10.0,
       ),
-      child: const Text(
-        'Login',
-        style: TextStyle(
+      child: Text(
+        text,
+        style: const TextStyle(
           color: Colors.black
         ),
       ),
